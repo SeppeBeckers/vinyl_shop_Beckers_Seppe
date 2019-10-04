700467acd055300087c5dd0ea959d45ea3f85f13
SeppeBeckers <r0749133@student.thomasmore.be>
2019-10-04 11:07:35 +0200

    Opdracht 1: <= Basic controllers

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,34 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+/*Route::get('/', function () {
+    //return view('welcome');
+    return 'the Vinyl shop';
+}); */
+
+/*Route::get('contact-us', function () {
+    //return 'Contact info';
+    return view('contact');
+}); */
+
+Route::view('/', 'home');
+Route::view('contact-us', 'contact');
+
+//Route::view('admin/records', 'admin.records.index');
+/*Route::get('admin/records', function (){
+    $records = [
+        'Queen - <b>Greatest Hits</b>',
+        'The Rolling Stones - <em>Sticky Fingers</em>',
+        'The Beatles - Abbey Road'
+    ];
+
+    return view('admin.records.index', [
+        'records' => $records
+    ]);
+});*/
+
+// New version with prefix and group
+Route::prefix('admin')->group(function () {
+    Route::redirect('/', 'admin/records');
+    Route::get('records', 'Admin\RecordController@index');
 });
